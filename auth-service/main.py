281fab5c17ac69d493bc457dc13cdb7049793f6b
Kiler56 <jpenao@eafit.edu.co>
2026-03-25 00:35:29 -0500

    feat: agrega frontend login y profile + ajustes en main

--- auth-service/main.py
+++ auth-service/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from database import engine
 from models import User
+from fastapi.middleware.cors import CORSMiddleware
 import auth
 
 User.metadata.create_all(bind=engine)
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
